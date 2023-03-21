7980742fc364ac67aa744615ce6861bf2769511d
Natalia Oliveira <nataliaferreiraoliveira1994@gmail.com>
2023-03-21 19:11:00 -0300

    add content

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 This repository contains sample files for studying Python with the
 [Fluent Python book](https://www.amazon.com.br/gp/product/B09WZJMMJP/ref=ppx_yo_dt_b_d_asin_image_o00?ie=UTF8&psc=1).
+
+- [Chapter 01](https://github.com/nfo94/fluentpython/tree/main/ch01)
+
+Obs: this is not an easy book, god damn. I'm losing it trying to understand all this stuff.
--- ch01/ch01.py
+++ ch01/ch01.py
@@ -1,4 +1,5 @@
 import collections
+from random import choice
 
 # namedtuple(): factory function for creating tuple subclasses with named fields
 # tuple: collection which is ordered and unchangeable, written with round brackets,
@@ -8,11 +9,18 @@ Card = collections.namedtuple("Card", ["rank", "suit"])
 
 class FrenchDeck:
     ranks = [str(n) for n in range(2, 11)] + list("JQKA")
+    # why split like this instead of just creating a list with the strings? is this about
+    # performance stuff?
     suits = "spades diamonds clubs hearts".split()
 
     def __init__(self):
+        # are we adding a variable to self, which is the own object? can i say this is a
+        # new property?
         self._cards = [Card(rank, suit) for suit in self.suits for rank in self.ranks]
 
+    # i've read somewhere that ovewriting special methods is not a good idea. is this it?
+    # or it got nothing to do with it and we're actually creating special methods for this
+    # new object called FreanchDeck?
     def __len__(self):
         return len(self._cards)
 
@@ -22,3 +30,14 @@ class FrenchDeck:
 
 beer_card = Card("7", "diamonds")
 print(beer_card)
+deck = FrenchDeck()
+print(choice(deck))
+print(choice(deck))
+
+# "Because our __getitem__ delegates to [] operator of self._cards, our deck automatically
+# supports slicing". ???
+
+print(deck[:3])
+
+# "Because our __getitem__ delegates to [] operator of self._cards, our deck automatically
+# supports slicing". ???
